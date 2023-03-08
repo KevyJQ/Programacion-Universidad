69f19081e287e9a6f985d025304de38ace6d2941
Kevy <kevy.bryan@gmail.com>
2023-03-07 18:08:58 -0600

    Movemos la carpeta donde se encuentra el codigo una carpeta antes

---
+++ Ingenieria-software-semana1/src/main/java/anahuac/intf/ScanService.java
@@ -0,0 +1,9 @@
+package anahuac.intf;
+
+public interface ScanService {
+
+	public int pedirInt(String msg, String errorMsg);
+
+	public int pedirNumeroEntreRango(String msg, String errorMsg, int limiteInferiorInclusivo,
+			int limiteSuperiorInclusivo);
+}
---
+++ Ingenieria-software-semana1/src/main/java/anahuac/intf/Telefono.java
@@ -0,0 +1,16 @@
+package anahuac.intf;
+
+import anahuac.model.TipoTelefono;
+
+public interface Telefono {
+
+	public void pedirDatos(TipoTelefono telefono);
+
+	public void telefonoFijo(TipoTelefono telefono);
+
+	public void telefonoCelular(TipoTelefono telefono);
+
+	public void imprirmirDatos(TipoTelefono telefono);
+
+
+}
